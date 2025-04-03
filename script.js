749b88e57f5169727a2df4199c16622e8e3611d3
mjkuranda <kuranda.marek@gmail.com>
2025-04-03 17:01:33 +0200

    Refined form

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const countryInput = document.getElementById('country');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
+let choices = null;
 
 function handleClick() {
     clickCount++;
     clicksInfo.innerText = clickCount;
 }
 
+function handleClickVAT() {
+    document.getElementById('vat-section').style.display = this.checked ? 'block' : 'none';
+}
+
 async function fetchAndFillCountries() {
+    choices = new Choices(countryInput, {
+        searchEnabled: true,
+        shouldSort: false, // Zachowuje kolejność
+        placeholder: true,
+        placeholderValue: "Wybierz kraj"
+    });
+
     try {
         const response = await fetch('https://restcountries.com/v3.1/all');
         if (!response.ok) {
@@ -18,7 +30,17 @@ async function fetchAndFillCountries() {
         }
         const data = await response.json();
         const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        // countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+
+        choices.setChoices(
+            countries.map(country => ({
+                value: country,
+                label: country
+            })),
+            'value',
+            'label',
+            true // Wymusza odświeżenie
+        );
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -28,8 +50,11 @@ function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
-            const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            const countryName = data.country;
+            
+            // document.getElementById('country').value = countryName;
+            choices.setChoiceByValue(countryName);
+            getCountryCode(countryName);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -48,7 +73,8 @@ function getCountryCode(countryName) {
     })
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+
+        document.getElementById('countryCode').value = countryCode;
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
@@ -59,6 +85,12 @@ function getCountryCode(countryName) {
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
+    document.getElementById('vatUE').addEventListener('change', handleClickVAT);
 
+    document.addEventListener('DOMContentLoaded', function () {
+        new Choices('#country', { searchEnabled: true, removeItemButton: false });
+    });   
+
+    getCountryByIP();
     fetchAndFillCountries();
 })()
